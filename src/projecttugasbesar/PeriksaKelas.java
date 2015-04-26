741b008df31541c3fe51c629c5a3a2f35405cb75
201410370311031 <dedipurniawan21@gmail.com>
2015-04-26 12:10:39 +0700

    Tugas Besar (Program Inventaris)

---
+++ src/projecttugasbesar/PeriksaKelas.java
@@ -0,0 +1,17 @@
+package projecttugasbesar;
+import java.util.*;
+import java.util.Scanner;
+
+public class PeriksaKelas {
+    public static void main (String[] dedot){
+    AnalisaRuangKelas dot = new AnalisaRuangKelas();
+    
+    dot.IdentitasRuangKelas();
+    dot.KondisiRuangKelas();
+    dot.DataKelas();
+    dot.cetakDataKelas();
+    dot.LingkunganRuangKelas();
+    dot.KebersihanRuangKelas();
+    
+   }
+}
